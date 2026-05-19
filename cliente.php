abadf3cef19b0a04c0ea757421e477387b871a91
hillarys2-del <suquinhatrevosa@gmail.com>
2026-05-19 08:55:11 -0300

    Implement getter and setter methods in cliente.php
    Added getter and setter methods for nome, email, telefone, and senha.

--- cliente.php
+++ cliente.php
@@ -13,15 +13,40 @@ class Cliente {
         $this->email = $email;
         $this->telefone = $telefone;
         $this->senha = $senha;
-    }
+        
+   }
+    
+   public function getNome() {
+    return $this->nome;
+   }
 
-    public function getNome() {
-        return $this->nome;
-    }
+   public function getEmail() {
+    return $this->email;
+   }
 
-    public function getEmail() {
-        return $this->email;
-    }
+   public function getTelefone() {
+    return $this->telefone;
+   }
+
+   public function getSenha() {
+    return $this->senha;
+   }
+
+   public function setNome($nome) {
+    $this->nome = $nome;
+   }
+
+   public function setEmail($email) {
+    $this->email = $email;
+   }
+
+   public function setTelefone($telefone) {
+    $this->telefone = $telefone;
+   }
+
+   public function setSenha($senha) {
+    $this->senha = $senha;
+   }
 
     public function login() {
 
